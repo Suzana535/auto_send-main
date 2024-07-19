4ddd8f3ef1b51d8b84fd42c3dc805c9a6621ce35
Дмитрий <dolgov.mitia2016@yandex.ru>
2024-07-19 16:54:10 +0700

    Обработка исключений и логирование ошибок

--- updater.py
+++ updater.py
@@ -15,30 +15,20 @@ def update_db(server,username,password):
     connection=sqlite3.connect("my_db.db")
     cursor=connection.cursor()
     for item in data:
-        cursor.execute("select count(*) from users where id_delo not null")
+        cursor.execute("select max(id) from users where id_delo not null")
         id=int(cursor.fetchall()[0][0])+1
         cursor.execute("select id_delo from users where id_delo not null")
         users=cursor.fetchall()
         users=list(map(lambda x: x[0],users))
-        print(users)
-        # if item[0] not in users:
-        #     cursor.execute(f'''
-        #     update users
-        #     set id_delo={item[0]}, firstname="{item[1]}",
-        #     lastname="{item[2]}", email="{item[3]}"
-        #     where id_delo is Null 
-        #     and id={id}
-        #     ''')
-        #     id+=1
-        #     connection.commit()
+        print(id)
         if item[0] not in users:
             cursor.execute(f'''
-            insert  users
+            update users
             set id_delo={item[0]}, firstname="{item[1]}",
             lastname="{item[2]}", email="{item[3]}"
             where id_delo is Null 
             and id={id}
-            ''')
+             ''')
             id+=1
             connection.commit()
     connection.close()
